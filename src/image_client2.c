23bbd85569a7409b05fd3dbb9c19719920257145
WoogiBoogi1129 <hwoogiboogi1129@gmail.com>
2023-05-10 14:22:25 +0900

    add

--- src/image_client2.c
+++ src/image_client2.c
@@ -6,13 +6,13 @@
 #include <unistd.h>
 
 #define SERVER_IP "192.168.64.2"
-#define SERVER_PORT "9001"
+#define SERVER_PORT 9001
 #define BUF_SIZE 1024
 
 int main(int argc, char const *argv[])
 {
     int sockfd;
-    char *filename = "image.jpg";
+    char *filename = "sex.jpg";
     char buffer[BUF_SIZE];
     struct sockaddr_in server_addr;
     FILE *fp;
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[])
     // 서버 주소, 포트 설정
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(atoi(SERVER_PORT));
+    server_addr.sin_port = htons(SERVER_PORT);
     if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0)
     {
         perror("inet_pton failed");
@@ -62,7 +62,12 @@ int main(int argc, char const *argv[])
 
         int packet_size = packet_end - packet_start;
         memcpy(buffer, &image_data[packet_start], packet_size);
-        bytes_sent += sendto(sockfd, buffer, packet_size, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (sendto(sockfd, buffer, packet_size, 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        {
+            perror("sendto failed");
+            exit(EXIT_FAILURE);
+        }
+        bytes_sent += packet_size;
         printf("Sent packet %d/%d\n", i + 1, num_packets);
     }
 
